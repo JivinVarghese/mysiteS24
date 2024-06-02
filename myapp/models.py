4a7525949b08123a61428acbca2eeeccfe9e53cf
Jivin Varghese <jivinvarghese007@gmail.com>
2024-06-02 16:04:05 -0400

    completed lab 4

--- myapp/models.py
+++ myapp/models.py
@@ -7,6 +7,11 @@ class Publisher(models.Model):
     name = models.CharField(max_length=200)
     website = models.URLField()
     city = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=50, default='USA')  # New field
+
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     CATEGORY_CHOICES = [
@@ -21,3 +26,39 @@ class Book(models.Model):
     num_pages = models.PositiveIntegerField(default=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     publisher = models.ForeignKey(Publisher, related_name='books', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)  # New field
+    def __str__(self):
+        return self.title
+class Member(User):
+    STATUS_CHOICES = [
+        (1, 'Regular member'),
+        (2, 'Premium Member'),
+        (3, 'Guest Member'),
+    ]
+
+    status = models.IntegerField(choices=STATUS_CHOICES, default=1)
+    address = models.CharField(max_length=300, blank=True, null=True)
+    city = models.CharField(max_length=20,default='Windsor')
+    province=models.CharField(max_length=2, default='ON')
+    last_renewal = models.DateField(default=timezone.now)
+    auto_renew = models.BooleanField(default=True)
+    borrowed_books = models.ManyToManyField(Book, blank=True)
+
+    def __str__(self):
+        return self.username
+# New Order model
+class Order(models.Model):
+    ORDER_TYPE_CHOICES = [
+        (0, 'Purchase'),
+        (1, 'Borrow'),
+    ]
+
+    books = models.ManyToManyField(Book)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    order_type = models.IntegerField(choices=ORDER_TYPE_CHOICES, default=1)
+    order_date = models.DateField(default=timezone.now)
+    def __str__(self):
+        # return self.get_order_type_display()
+        return f"{self.get_order_type_display()} by {self.member.username} on {self.order_date}"
+    def total_items(self,):
+        return self.books.count()
