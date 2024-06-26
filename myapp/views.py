ca2ff0092f11dc9bc0f211d2ade01783cac21f1d
Harsh Sachala <sach46229@gmail.com>
2024-06-26 08:59:09 -0400

    Lab 6

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,15 @@
-
-
 from django.shortcuts import render
+from .models import Book
 
 def home(request):
     return render(request, 'home.html')
+def index(request):
+    booklist = Book.objects.all().order_by('id')[:10]
+    return render(request, 'myapp/index0.html', {'booklist': booklist})
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'myapp/about0.html')
 
+def detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'myapp/detail0.html', {"book": book})
